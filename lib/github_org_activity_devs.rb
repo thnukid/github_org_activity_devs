b3d97bf6f8ae25d65114b30b8ce38f76e3957f5f
Eric Bartholemy <eric@kabisa.nl>
2019-02-17 19:19:22 +0100

    stub requests so that there is no need for vcr gem

--- lib/github_org_activity_devs.rb
+++ lib/github_org_activity_devs.rb
@@ -12,17 +12,44 @@ module GithubOrgActivityDevs
     def initialize
       Dotenv.load
       ActiveRecord::Base.establish_connection(db_configuration['development'])
-      Octokit.configure do |c|
-        c.auto_paginate = true
-      end
       @client = Octokit::Client.new(access_token: github_token)
+      @client.auto_paginate = true
     end
 
     def developers
-      client.team_members(team_members_id)
+      @developers ||= client.team_members(team_members_id)
+    end
+
+    def activity
+      @activity ||= developers.each_with_object({}) do |developer, hash|
+        hash[developer.login] = client.user_events(developer.login)
+      end
+    end
+
+    # { developer: [{ repos }] }
+    def watch_events
+      @watch_events = activity.each_with_object({}).each do |stream, hash|
+        watch_events = stream.last.select { |x| x[:type] == 'WatchEvent' }
+        developer = stream.first
+        hash[developer] = watch_events
+      end
+    end
+
+    def summary
+      watch_events.each do |developer|
+        name = developer.first
+        repos = developer.last.map do |x|
+          ['https://github.com/', x[:repo][:name]].join
+        end
+        output_console(name, repos)
+      end
     end
 
     private
+
+    def output_console(name, repos)
+      puts "#{name} (#{repos.count}) -> #{repos.join(', ')}"
+    end
 
     def github_token
       ENV.fetch('GITHUB_OAUTH_TOKEN')
--- spec/github_org_activity_devs_spec.rb
+++ spec/github_org_activity_devs_spec.rb
@@ -1,4 +1,6 @@
+# rubocop:disable Metrics/ModuleLength
 module GithubOrgActivityDevs
+  # rubocop:disable Metrics/BlockLength
   RSpec.describe Main do
     it 'has a version number' do
       expect(GithubOrgActivityDevs::VERSION).not_to be nil
@@ -32,22 +34,105 @@ module GithubOrgActivityDevs
 
       context 'ocotokit' do
         before do
-          allow(ENV).to receive(:fetch).with('GITHUB_OAUTH_TOKEN').and_return('my-token')
-          allow(ENV).to receive(:fetch).with('GITHUB_TEAM_MEMBERS_ID').and_return(team_members_id)
-          allow(Octokit::Client).to receive(:team_members).and_return([{}])
+          allow(ENV).to receive(:fetch).with('GITHUB_OAUTH_TOKEN')
+                                       .and_return('my-token')
+          allow(Octokit::Client).to receive(:new)
+            .and_call_original
         end
 
         it 'loads the client with oauth token' do
-          expect(Octokit::Client).to receive(:new).with(access_token: 'my-token')
+          expect(Octokit::Client).to receive(:new)
+            .with(access_token: 'my-token')
           subject
         end
+      end
 
-        let(:team_members_id) { '000000' }
-        it 'loads team_members' do
-          expect(Octokit::Client).to receive(:team_members).with(team_members_id)
-          subject.developers
+      context '.developers' do
+        before do
+          allow_any_instance_of(Octokit::Client).to receive(:team_members)
+            .and_return([double(login: 'developer')])
+        end
+
+        it 'loads team_members developers' do
+          expect(subject.developers.class).to eq Array
+          expect(subject.developers.count).to eq 1
+        end
+      end
+
+      context '.activity' do
+        before do
+          allow(subject).to receive(:developers).and_return(stub_developer)
+        end
+
+        let(:stub_developer) { [double(login: 'developer1')] }
+
+        it 'calls user_events' do
+          expect_any_instance_of(Octokit::Client).to receive(:user_events)
+            .with('developer1')
+          subject.activity
+        end
+
+        context 'returns user_events' do
+          before do
+            allow_any_instance_of(Octokit::Client).to receive(:user_events)
+              .and_return(events)
+          end
+
+          let(:events) do
+            [{}]
+          end
+
+          it 'includes the activity of a group member' do
+            expect(subject.activity.include?('developer1')).to eq true
+          end
+
+          it 'includes the activity_events' do
+            expect(subject.activity.fetch('developer1').last).to eq({})
+          end
+        end
+      end
+
+      context '.watch_events' do
+        before do
+          allow(subject).to receive(:activity).and_return(stub_user_events)
+        end
+
+        let(:stub_user_events) do
+          [
+            ['developer1', [{ type: 'WatchEvent' }]],
+            ['empty', [{ type: 'PushEvent' }]]
+          ]
+        end
+
+        it 'finds the developers' do
+          expect(subject.watch_events.fetch('developer1').class).to eq Array
+          expect(subject.watch_events.fetch('empty').class).to eq Array
+        end
+
+        it 'finds the WatchEvent' do
+          expect(subject.watch_events.fetch('developer1').count).to eq 1
+          expect(subject.watch_events.fetch('empty').count).to eq 0
+        end
+      end
+
+      context '.summary' do
+        before do
+          allow(subject).to receive(:watch_events).and_return(stub_watch_events)
+        end
+        let(:stub_watch_events) do
+          [
+            ['developer1', [{ repo: { name: 'test/test-repo' } }]]
+          ]
+        end
+
+        it 'prints to console' do
+          expect(subject).to receive(:output_console)
+            .with('developer1', ['https://github.com/test/test-repo'])
+          subject.summary
         end
       end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
+# rubocop:enable Metrics/ModuleLength
