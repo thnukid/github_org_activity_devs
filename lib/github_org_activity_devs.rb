e6428ea28303d5a2c601f0f7348155250ce2a099
Eric Bartholemy <eric@kabisa.nl>
2019-02-17 11:41:19 +0100

    run bundle gem github_org_activity_devs --test=rspec --bin

---
+++ lib/github_org_activity_devs.rb
@@ -0,0 +1,5 @@
+require "github_org_activity_devs/version"
+
+module GithubOrgActivityDevs
+  # Your code goes here...
+end
---
+++ spec/github_org_activity_devs_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GithubOrgActivityDevs do
+  it "has a version number" do
+    expect(GithubOrgActivityDevs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
