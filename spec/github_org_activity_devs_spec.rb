fcb46579643927faffbd8e68accf8ea0b9952a25
Eric Bartholemy <eric@kabisa.nl>
2019-02-17 13:12:35 +0100

    add bundler require and establish activerecord connection from config.yml

--- spec/github_org_activity_devs_spec.rb
+++ spec/github_org_activity_devs_spec.rb
@@ -1,9 +1,25 @@
-RSpec.describe GithubOrgActivityDevs do
-  it "has a version number" do
-    expect(GithubOrgActivityDevs::VERSION).not_to be nil
-  end
+module GithubOrgActivityDevs
+  RSpec.describe Main do
+    it 'has a version number' do
+      expect(GithubOrgActivityDevs::VERSION).not_to be nil
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    context 'intialize' do
+      subject { described_class.new }
+      let(:development_config) { {} }
+      let(:yaml_config_hash) do
+        { 'development' => development_config }
+      end
+
+      before do
+        allow(YAML).to receive(:load).and_return(yaml_config_hash)
+      end
+
+      it 'will establish a connection to the database' do
+        expect(ActiveRecord::Base).to receive(:establish_connection)
+          .with(development_config)
+        subject
+      end
+    end
   end
 end
